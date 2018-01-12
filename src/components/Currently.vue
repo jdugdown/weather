75edd949043f2a8568074e762c686d27a7c7d46e
Joshua Downey <downey.josh@gmail.com>
2018-01-12 15:35:16 -0500

    replace default component

---
+++ src/components/Currently.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="currently">
+    <p><strong>time:</strong> {{ forecast.time }}</p>
+    <p><strong>summary:</strong> {{ forecast.summary }}</p>
+    <p><strong>icon:</strong> {{ forecast.icon }}</p>
+    <p><strong>temperature:</strong> {{ forecast.temperature }}</p>
+    <p><strong>apparentTemperature:</strong> {{ forecast.apparentTemperature }}</p>
+    <p><strong>dewPoint:</strong> {{ forecast.dewPoint }}</p>
+    <p><strong>humidity:</strong> {{ forecast.humidity }}</p>
+    <p><strong>pressure:</strong> {{ forecast.pressure }}</p>
+    <p><strong>windSpeed:</strong> {{ forecast.windSpeed }}</p>
+    <p><strong>windBearing:</strong> {{ forecast.windBearing }}</p>
+    <p><strong>cloudCover:</strong> {{ forecast.cloudCover }}</p>
+    <p><strong>unIndex:</strong> {{ forecast.unIndex }}</p>
+    <p><strong>visibility:</strong> {{ forecast.visibility }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Currently',
+  props: [
+    'forecast',
+  ]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
