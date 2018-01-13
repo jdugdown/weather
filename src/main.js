e41b27ad069edaf8f86511ec24b3796f4a7a4743
Joshua Downey <downey.josh@gmail.com>
2018-01-13 17:58:03 -0500

    add date formatting filter

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import moment from 'moment'
 
 Vue.config.productionTip = false
 
@@ -27,5 +28,12 @@ Vue.filter('percent', function (value) {
 Vue.filter('round', function (value) {
   if (!value) return ''
   value = Math.round(value)
+  return value
+})
+
+Vue.filter('formatDate', function (value) {
+  if (!value) return ''
+  value = new Date(value*1000);
+  value = moment(value).format('ddd DD')
   return value
 })
