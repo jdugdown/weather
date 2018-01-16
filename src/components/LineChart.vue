ba49afbc4b9cac995f8f20be3d6e1afa78e50f90
Joshua Downey <downey.josh@gmail.com>
2018-01-16 18:21:53 -0500

    add linechart component

---
+++ src/components/LineChart.vue
@@ -0,0 +1,37 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  name: 'LineChart',
+  extends: Line,
+  props: [
+    'hours',
+    'temps'
+  ],
+  mounted () {
+    this.renderChart(
+      {
+        labels: this.hours,
+        datasets: [
+          {
+            label: 'Temperature',
+            backgroundColor: 'rgba(255, 255, 255, .5)',
+            data: this.temps
+          }
+        ]
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+      }
+    )
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
