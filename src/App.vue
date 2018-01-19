02c1ee253ebc41f493ac333bc50f44dcee9cdec6
Joshua Downey <downey.josh@gmail.com>
2018-01-19 11:18:04 -0500

    add cynamic page background

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,31 @@
   <div id="app">
     <div id="loading" v-if="loading"></div>
 
-    <div class="container" v-else>
+    <div v-else>
       <div v-if="errors && errors.length">
-        <div class="message message-error" v-for="error in errors"><strong>Error:</strong> {{ error.message }}</div>
+        <div class="container">
+          <div class="message message-error" v-for="error in errors"><strong>Error:</strong> {{ error.message }}</div>
+        </div>
       </div>
 
-      <div v-else>
-        <div class="message" :class="'message-' + alert.severity" v-for="alert in forecast.alerts">
-          <div><strong>{{ alert.title }}</strong></div>
-          <div>{{ alert.description }}</div>
+      <div class="bg" :class="background" v-else>
+        <div class="container">
+          <div class="message" :class="'message--' + alert.severity" v-for="alert in forecast.alerts">
+            <div class="message__title"><strong>{{ alert.title }}</strong></div>
+            <div class="message__description">{{ alert.description }}</div>
+          </div>
+
+          <h1>Somecity, State</h1>
+
+          <Currently :forecast="forecast.currently"></Currently>
+          <Today :forecast="forecast.daily.data[0]"></Today>
+          <Hourly :forecast="forecast.hourly"></Hourly>
+          <Weekly :forecast="forecast.daily"></Weekly>
+
+          <p class="attribution">
+            <a href="https://darksky.net/poweredby/" target="_blank">Powered by Dark Sky</a>
+          </p>
         </div>
-
-        <h1>Somecity, State</h1>
-
-        <Currently :forecast="forecast.currently"></Currently>
-        <Today :forecast="forecast.daily.data[0]"></Today>
-        <Hourly :forecast="forecast.hourly"></Hourly>
-        <Weekly :forecast="forecast.daily"></Weekly>
       </div>
     </div>
   </div>
@@ -26,6 +34,7 @@
 
 <script>
 import axios from 'axios'
+import moment from 'moment'
 import Currently from './components/Currently'
 import Today from './components/Today'
 import Hourly from './components/Hourly'
@@ -48,11 +57,7 @@ export default {
       forecast: []
     }
   },
-  created () {
-    console.log('created')
-  },
   mounted () {
-    console.log('mounted')
     this.getForecast()
   },
   methods: {
@@ -66,6 +71,30 @@ export default {
           this.errors.push(error)
           this.loading = false
         })
+    }
+  },
+  computed: {
+    background: function () {
+      var background = ''
+      var daytime = 'night'
+      var clouds = 'clear'
+
+      var now = this.forecast.currently.time
+      var sunrise = this.forecast.daily.data[0].sunriseTime
+      var sunset = this.forecast.daily.data[0].sunsetTime
+      var cloudCover = this.forecast.currently.cloudCover
+
+      if (sunrise <= now && now <= sunset) {
+        daytime = 'day'
+      }
+
+      if (cloudCover > 0.5) {
+        clouds = 'clouds'
+      }
+
+      background = daytime + '-' + clouds
+
+      return background
     }
   }
 }
