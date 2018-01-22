69003f78c5a3e7b3b5c477f339557517ce08708e
Joshua Downey <downey.josh@gmail.com>
2018-01-22 15:17:17 -0500

    add geolocation feature

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,10 @@
           <div class="container">
             <form v-on:submit.prevent="searchForecast(searchTerm)">
               <input type="text" placeholder="City or Zip Code" v-model="searchTerm">
-              <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24"><path fill="white" d="M508.5 481.6l-129-129c-2.3-2.3-5.3-3.5-8.5-3.5h-10.3C395 312 416 262.5 416 208 416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c54.5 0 104-21 141.1-55.2V371c0 3.2 1.3 6.2 3.5 8.5l129 129c4.7 4.7 12.3 4.7 17 0l9.9-9.9c4.7-4.7 4.7-12.3 0-17zM208 384c-97.3 0-176-78.7-176-176S110.7 32 208 32s176 78.7 176 176-78.7 176-176 176z"/></svg></button>
+              <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24"><path fill="white" d="M508.5 468.9L387.1 347.5c-2.3-2.3-5.3-3.5-8.5-3.5h-13.2c31.5-36.5 50.6-84 50.6-136C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c52 0 99.5-19.1 136-50.6v13.2c0 3.2 1.3 6.2 3.5 8.5l121.4 121.4c4.7 4.7 12.3 4.7 17 0l22.6-22.6c4.7-4.7 4.7-12.3 0-17zM208 368c-88.4 0-160-71.6-160-160S119.6 48 208 48s160 71.6 160 160-71.6 160-160 160z"/></svg></button>
             </form>
+
+            <button type="button" name="findMe" @click="locateForecast()"><svg xmlns="http://www.w3.ord/2000/svg" viewBox="0 0 24 24" width="24px" height="24px"><path fill="white" d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M3.05,13H1V11H3.05C3.5,6.83 6.83,3.5 11,3.05V1H13V3.05C17.17,3.5 20.5,6.83 20.95,11H23V13H20.95C20.5,17.17 17.17,20.5 13,20.95V23H11V20.95C6.83,20.5 3.5,17.17 3.05,13M12,5A7,7 0 0,0 5,12A7,7 0 0,0 12,19A7,7 0 0,0 19,12A7,7 0 0,0 12,5Z" /></svg></button>
           </div>
         </div>
 
@@ -130,6 +132,47 @@ export default {
           this.errors.push(error)
           this.loading = false
         })
+    },
+    locateForecast () {
+      this.loading = true
+
+      var geoOptions = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      }
+
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          var location = pos.coords.latitude + ',' + pos.coords.longitude
+
+          var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&language=en'
+
+          axios.get(url)
+            .then(location => {
+              if (location.data.status == 'OK') {
+                this.formattedAddress = location.data.results[0].formatted_address
+              } else {
+                var geoError = {message: 'Location not found.'}
+                this.errors.push(geoError)
+                this.loading = false
+              }
+            })
+            .catch(error => {
+              this.errors.push(error)
+              this.loading = false
+            })
+
+          this.getForecast(location)
+        },
+
+        error => {
+          this.errors.push(error)
+          this.loading = false
+        },
+
+        geoOptions
+      )
     }
   },
   computed: {
